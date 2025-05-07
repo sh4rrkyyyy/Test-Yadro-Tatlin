96a43d6a9108cbf9ba82e01b9dfd8f663a731411
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-05-07 11:26:41 +0300

    update readme

---
+++ include/constants.h
@@ -0,0 +1,5 @@
+namespace constants {
+  inline const std::string PathHelpTape0 = "./tmp/tape0";
+  inline const std::string PathHelpTape1 = "./tmp/tape1";
+  inline constexpr size_t MaxReadSize = 1024;
+}
