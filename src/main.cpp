a708b3adf2dcb47647cb97a0fa06e78f1cd7cd50
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-05-08 13:42:42 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include "config.h"
+#include "constants.h"
 #include "file-tape.h"
 #include "sort.h"
 #include <algorithm>
 #include <chrono>
 #include <cstdlib>
 #include <exception>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <optional>
 #include <vector>
-#include <filesystem>
-
-const std::string path_help_tape0 = "./tmp/tape0";
-const std::string path_help_tape1 = "./tmp/tape1";
 
 int main(int argc, char **argv) {
   if (argc != 4) {
@@ -24,16 +22,17 @@ int main(int argc, char **argv) {
     std::cerr << "No config file " + std::string(argv[3]) << std::endl;
     exit(EXIT_FAILURE);
   }
- 
+
   std::filesystem::create_directories("./tmp");
   try {
     Config cfg(in_config);
     FileTape in_tape(argv[1], cfg);
     FileTape out_tape(argv[2], cfg, 1);
-    FileTape tape0(path_help_tape0, cfg, 1);
-    FileTape tape1(path_help_tape1, cfg, 1);
+    FileTape tape0(constants::PathHelpTape0, cfg, 1);
+    FileTape tape1(constants::PathHelpTape1, cfg, 1);
     Sort(in_tape, out_tape, tape0, tape1);
-    
+    std::filesystem::remove_all("./tmp");
+
   } catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
     exit(EXIT_FAILURE);
